a9aa24447187bd252a3f1991ce284ffaa9f5f4ca
Przemysław Górecki <minutnik75@gmail.com>
2024-05-03 06:34:33 +0200

    The following functions have been implemented: driver_setup fill_in_registration_form

--- SRC/seleniumdemotest.py
+++ SRC/seleniumdemotest.py
@@ -1,47 +1,59 @@
 from selenium import webdriver
 import time
 import pytest
+from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.common.keys import Keys
 import random
 
-user_login = str(random.randint(1, 2)) + "karikinx@gmail.com"
-password = "#1QAZwsx#1"
-wrong_password = "#1QAZwsx#111"
+CORRECT_PASSWORD = "#1QAZwsx#1"
+WRONG_PASSWORD = "#1QAZwsx#111"
 
 
-def test_setup():
+def generate_login():
+    return str(random.randint(1, 5)) + "karikinx@gmail.com"
+
+
+def driver_setup():
     driver = webdriver.Chrome()
     driver.maximize_window()
     driver.get("http://seleniumdemo.com/?page_id=7")
     return driver
 
 
-def test_register_passed():
-    driver = test_setup()
+def fill_in_registration_form(driver, login, password):
     driver.find_element(By.ID, "reg_email").click()
-    driver.find_element(By.ID, "reg_email").send_keys(user_login)
+    driver.find_element(By.ID, "reg_email").clear()
+    driver.find_element(By.ID, "reg_email").send_keys(login)
+
     driver.find_element(By.ID, "reg_password").click()
+    driver.find_element(By.ID, "reg_password").clear()
     driver.find_element(By.ID, "reg_password").send_keys(password)
-    print(user_login)
+
+
+def test_register_passed():
+    driver = driver_setup()
+    user_login = generate_login()
+    fill_in_registration_form(driver, user_login, CORRECT_PASSWORD)
     driver.find_element(By.NAME, "register").click()
-    if not ("An account is already registered with your email address. Please log in."
-    in driver.find_element(By.XPATH, "//ul[@class='woocommerce-error']//li").text):
-        print("hello")
+    time.sleep(5)
+    try:
+        while ("An account is already registered with your email address. Please log in."
+               in driver.find_element(By.XPATH, "//ul[@class='woocommerce-error']//li")
+                       .text):
+            print("Error message found")
+            driver.close()
+
+            driver = driver_setup()
+            user_login = generate_login()
+            fill_in_registration_form(driver, user_login, CORRECT_PASSWORD)
+            driver.find_element(By.NAME, "register").click()
+            time.sleep(5)
+    except NoSuchElementException as e:
+        print("Error message not found")
         assert driver.find_element(By.LINK_TEXT, "Logout").is_displayed()
-    else:
-        print("dddd")
-    #     driver = test_setup()
-    #     driver.find_element(By.ID, "reg_email").click()
-    #     driver.find_element(By.ID, "reg_email").send_keys(str(random.randint(1, 2)) + "karikinx@gmail.com")
-    #     driver.find_element(By.ID, "reg_password").click()
-    #     driver.find_element(By.ID, "reg_password").send_keys(password)
-    #     driver.find_element(By.NAME, "register").click()
-    #     time.sleep(5)
-
-
-    time.sleep(3)
+        time.sleep(5)
 
 
 # def test_log_in_passed():
